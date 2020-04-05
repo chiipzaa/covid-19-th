e8d18063f37379f3a33144554ffbcb14b8f65c51
teerasit arttantra <teerasit.art@gmail.com>
2020-04-05 21:44:15 +0700

    update readme

--- src/lib/db.php
+++ src/lib/db.php
@@ -2,7 +2,7 @@
 	  
     define("DB_HOST","localhost");
     define("DB_PORT","3306");
-    define("DB_NAME","hdc");
+    define("DB_NAME","dc");
     define("DB_USERNAME","root");
     define("DB_PASSWORD","root");
     define("DB_ISO","utf8");
